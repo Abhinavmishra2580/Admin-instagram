3470effa8b6cdb6c24aeb891ae41d446f75182d2
Black venom <abhinavpandit1470@gmail.com>
2025-02-09 19:30:10 +0530

    Delete admin script.js

--- admin script.js
+++
@@ -1,9 +0,0 @@
-// This script will be shared between admin-panel.html and public-user-info.html
-
-// Retrieve the public user's information from the local storage
-const publicUsername = localStorage.getItem('public-username');
-const publicPassword = localStorage.getItem('public-password');
-
-// Display the public user's information
-document.getElementById('username-display').textContent = `Public User Username: ${publicUsername}`;
-document.getElementById('password-display').textContent = `Public User Password: ${publicPassword}`;
